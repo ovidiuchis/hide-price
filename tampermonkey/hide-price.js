c90d0476c411fb067ea35791d2409d127294a451
Vlad <vlad.rusanescu@gmail.com>
2017-03-09 12:14:14 +0200

    Optimize code

--- tampermonkey/hide-price.js
+++ tampermonkey/hide-price.js
@@ -6,10 +6,13 @@
 // @author       You
 // @match        http*://*.emag.ro/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
 (function() {
     'use strict';
+
+    console.log(document);
 
     var classesArr = [
         "emg-carousel-price",
@@ -20,32 +23,18 @@
         "widget_product_initial_price",
         "widget_product_price",
         "emg-fluid-fav-price-area",
-        "emg-mini-cart-item-price"
+        "emg-mini-cart-item-price",
+        "product-price"
     ];
 
     var styleElement = document.createElement("style");
-    styleElement.innerHTML = ".hide-price-hidden { position: relative; display: none !important;} .hide-price-hidden:before { content: 'Pret ascuns'; position: absolute; background-color: #005eb8; border-radius: 4px; z-index: 9999; transform: rotate(2.5deg); left: -10px; color: white; font-size: 150%; padding: 8%; }";
-    document.getElementsByTagName("body")[0].appendChild(styleElement);
 
     var getClasses = function(className) {
-        return Array.prototype.map.call(document.getElementsByClassName(className), setHideClass);
+        styleElement.innerHTML += "." + className + " { display: none !important; } ";
     };
 
-    var setHideClass = function(priceElement) {
-        if (priceElement.classList.contains("hide-price-hidden") === false) {
-            return priceElement.classList.add("hide-price-hidden");
-        }
-    };
+    document.getElementsByTagName("head")[0].appendChild(styleElement);
 
-    var hidePrice = function(){
-        console.log('asd');
-        Array.prototype.map.call(classesArr, getClasses);
-    };
-
-    var hidePriceInterval = setInterval(hidePrice, 1);
-
-    setTimeout(function(){clearInterval(hidePriceInterval);}, 5000);
-
-    var hidePriceIntervalSlow = setInterval(hidePrice, 1000);
+    Array.prototype.map.call(classesArr, getClasses);
 
 })();
